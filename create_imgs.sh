00e76c856fa8c505184bb514029724daf7d80a55
Jorge Sallum <jorge@hedra.com.br>
2024-10-14 20:39:40 -0300

    subindo as imagens finais

--- create_imgs.sh
+++ create_imgs.sh
@@ -1,5 +1,6 @@
 # echo "remover pastas ./img"
 
+# rm ./PNLD2026-001_MIOLO/img -rf
 # rm ./PNLD2026-002_MIOLO/img -rf
 # rm ./PNLD2026-003_MIOLO/img -rf
 # rm ./PNLD2026-004_MIOLO/img -rf
@@ -11,9 +12,11 @@
 # rm ./PNLD2026-012_MIOLO/img -rf
 # rm ./PNLD2026-013_MIOLO/img -rf
 # rm ./PNLD2026-014_MIOLO/img -rf
+# rm ./PNLD2026-016_MIOLO/img -rf
 # rm ./PNLD2026-022_MIOLO/img -rf
 # rm ./PNLD2026-025_MIOLO/img -rf
 # rm ./PNLD2026-026_MIOLO/img -rf
+# rm ./PNLD2026-027_MIOLO/img -rf
 # rm ./PNLD2026-028_MIOLO/img -rf
 # rm ./PNLD2026-030_MIOLO/img -rf
 # rm ./PNLD2026-031_MIOLO/img -rf
@@ -25,80 +28,84 @@
 # rm ./PNLD2026-038_MIOLO/img -rf
 # rm ./PNLD2026-039_MIOLO/img -rf
 # rm ./PNLD2026-046_MIOLO/img -rf
-# rm ./PNLD2026-047_MIOLO/img -rf# 
-# rm ./PNLD2026-001_MIOLO/img -rf
-# rm ./PNLD2026-016_MIOLO/img -rf
+# rm ./PNLD2026-047_MIOLO/img -rf
 # rm ./PNLD2026-047_MIOLO/img -rf
 # rm ./PNLD2026-048_MIOLO/img -rf
-rm ./PNLD2026-049_MIOLO/img -rf
+# rm ./PNLD2026-049_MIOLO/img -rf
 # rm ./PNLD2026-050_MIOLO/img -rf
 
-edlab-pdfconverter -i PNLD2026-049_MIOLO.pdf -o ./temp/PNLD2026-049_MIOLO_temp.pdf -rt 				-d ./PNLD2026-049_MIOLO/img
 # edlab-pdfconverter -i PNLD2026-001_MIOLO.pdf -o ./temp/PNLD2026-001_MIOLO_temp.pdf -rt 				-d ./PNLD2026-001_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-016_MIOLO.pdf -o ./temp/PNLD2026-016_MIOLO_temp.pdf -rt -m '-36' 	-d ./PNLD2026-016_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-047_MIOLO.pdf -o ./temp/PNLD2026-047_MIOLO_temp.pdf -rt -m '-37' 	-d ./PNLD2026-047_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-048_MIOLO.pdf -o ./temp/PNLD2026-048_MIOLO_temp.pdf -rt  			-d ./PNLD2026-048_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-050_MIOLO.pdf -o ./temp/PNLD2026-050_MIOLO_temp.pdf -rt  			-d ./PNLD2026-050_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-002_MIOLO.pdf -o ./temp/PNLD2026-002_MIOLO_temp.pdf -rt  			-d ./PNLD2026-002_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-003_MIOLO.pdf -o ./temp/PNLD2026-003_MIOLO_temp.pdf -rt -m '-40'  	-d ./PNLD2026-003_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-004_MIOLO.pdf -o ./temp/PNLD2026-004_MIOLO_temp.pdf -rt   			-d ./PNLD2026-004_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-006_MIOLO.pdf -o ./temp/PNLD2026-006_MIOLO_temp.pdf -rt -m '-31'  	-d ./PNLD2026-006_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-009_MIOLO.pdf -o ./temp/PNLD2026-009_MIOLO_temp.pdf -rt -m '-37'  	-d ./PNLD2026-009_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-010_MIOLO.pdf -o ./temp/PNLD2026-010_MIOLO_temp.pdf -rt -m '-31'   	-d ./PNLD2026-010_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-010_MIOLO.pdf -o ./temp/PNLD2026-010_MIOLO_temp.pdf -rt -m '-31'  	-d ./PNLD2026-010_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-011_MIOLO.pdf -o ./temp/PNLD2026-011_MIOLO_temp.pdf -rt -m '-37'  	-d ./PNLD2026-011_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-012_MIOLO.pdf -o ./temp/PNLD2026-012_MIOLO_temp.pdf -rt -m '-31'   	-d ./PNLD2026-012_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-013_MIOLO.pdf -o ./temp/PNLD2026-013_MIOLO_temp.pdf -rt -m '-31'   	-d ./PNLD2026-013_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-014_MIOLO.pdf -o ./temp/PNLD2026-014_MIOLO_temp.pdf -rt -m '-31'   	-d ./PNLD2026-014_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-022_MIOLO.pdf -o ./temp/PNLD2026-022_MIOLO_temp.pdf -rt -m '-31'  	-d ./PNLD2026-022_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-025_MIOLO.pdf -o ./temp/PNLD2026-025_MIOLO_temp.pdf -rt -m '-31'  	-d ./PNLD2026-025_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-026_MIOLO.pdf -o ./temp/PNLD2026-026_MIOLO_temp.pdf -rt -m '-31'  	-d ./PNLD2026-026_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-028_MIOLO.pdf -o ./temp/PNLD2026-028_MIOLO_temp.pdf -rt -m '-31'   	-d ./PNLD2026-028_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-030_MIOLO.pdf -o ./temp/PNLD2026-030_MIOLO_temp.pdf -rt -m '-31'   	-d ./PNLD2026-030_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-031_MIOLO.pdf -o ./temp/PNLD2026-031_MIOLO_temp.pdf -rt -m '-31'   	-d ./PNLD2026-031_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-033_MIOLO.pdf -o ./temp/PNLD2026-033_MIOLO_temp.pdf -rt -m '-31'  	-d ./PNLD2026-033_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-034_MIOLO.pdf -o ./temp/PNLD2026-034_MIOLO_temp.pdf -rt -m '-31'   	-d ./PNLD2026-034_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-035_MIOLO.pdf -o ./temp/PNLD2026-035_MIOLO_temp.pdf -rt -m '-37'    	-d ./PNLD2026-035_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-036_MIOLO.pdf -o ./temp/PNLD2026-036_MIOLO_temp.pdf -rt -m '-31'  	-d ./PNLD2026-036_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-037_MIOLO.pdf -o ./temp/PNLD2026-037_MIOLO_temp.pdf -rt -m '-37'  	-d ./PNLD2026-037_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-038_MIOLO.pdf -o ./temp/PNLD2026-038_MIOLO_temp.pdf -rt -m '-31'  	-d ./PNLD2026-038_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-039_MIOLO.pdf -o ./temp/PNLD2026-039_MIOLO_temp.pdf -rt -m '-37'  	-d ./PNLD2026-039_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-046_MIOLO.pdf -o ./temp/PNLD2026-046_MIOLO_temp.pdf -rt -m '-37'  	-d ./PNLD2026-046_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-047_MIOLO.pdf -o ./temp/PNLD2026-047_MIOLO_temp.pdf -rt -m '-37'   	-d ./PNLD2026-047_MIOLO/img# 
-
-# # Páginas sem --remove-text
-edlab-pdfconverter -i PNLD2026-049_MIOLO.pdf -o ./temp/PNLD2026-049_MIOLO_temp2.pdf -p 1-3 			 -d ./PNLD2026-049_MIOLO/img
 # edlab-pdfconverter -i PNLD2026-001_MIOLO.pdf -o ./temp/PNLD2026-001_MIOLO_temp2.pdf -p 1-2 			 -d ./PNLD2026-001_MIOLO/img
+edlab-pdfconverter -i PNLD2026-002_MIOLO.pdf -o ./temp/PNLD2026-002_MIOLO_temp.pdf -rt -m '-31' 			-d ./PNLD2026-002_MIOLO/img
+edlab-pdfconverter -i PNLD2026-002_MIOLO.pdf -o ./temp/PNLD2026-002_MIOLO_temp2.pdf -p 1-2  -rt -m '-31'	-d ./PNLD2026-002_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-003_MIOLO.pdf -o ./temp/PNLD2026-003_MIOLO_temp.pdf -rt -m '-40'  	-d ./PNLD2026-003_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-003_MIOLO.pdf -o ./temp/PNLD2026-003_MIOLO_temp2.pdf -p 1-2 -m '-40'  -d ./PNLD2026-003_MIOLO/img
+edlab-pdfconverter -i PNLD2026-004_MIOLO.pdf -o ./temp/PNLD2026-004_MIOLO_temp.pdf -rt -m '-31'   			-d ./PNLD2026-004_MIOLO/img
+edlab-pdfconverter -i PNLD2026-004_MIOLO.pdf -o ./temp/PNLD2026-004_MIOLO_temp2.pdf -p 1-2  -m '-31' 		 -d ./PNLD2026-004_MIOLO/img
+edlab-pdfconverter -i PNLD2026-005_MIOLO.pdf -o ./temp/PNLD2026-005_MIOLO_temp.pdf   	-d ./PNLD2026-005_MIOLO/img
+edlab-pdfconverter -i PNLD2026-006_MIOLO.pdf -o ./temp/PNLD2026-006_MIOLO_temp.pdf -m '-31'  	-d ./PNLD2026-006_MIOLO/img
+edlab-pdfconverter -i PNLD2026-007_MIOLO.pdf -o ./temp/PNLD2026-007_MIOLO_temp.pdf -rt   	-d ./PNLD2026-007_MIOLO/img
+edlab-pdfconverter -i PNLD2026-007_MIOLO.pdf -o ./temp/PNLD2026-007_MIOLO_temp.pdf -p 1-3   	-d ./PNLD2026-007_MIOLO/img
+edlab-pdfconverter -i PNLD2026-008_MIOLO.pdf -o ./temp/PNLD2026-008_MIOLO_temp.pdf -rt   	-d ./PNLD2026-008_MIOLO/img
+edlab-pdfconverter -i PNLD2026-008_MIOLO.pdf -o ./temp/PNLD2026-008_MIOLO_temp.pdf -p 1-3   	-d ./PNLD2026-008_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-009_MIOLO.pdf -o ./temp/PNLD2026-009_MIOLO_temp.pdf -rt -m '-37'  	-d ./PNLD2026-009_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-009_MIOLO.pdf -o ./temp/PNLD2026-009_MIOLO_temp2.pdf -p 1-2 -m '-37'  -d ./PNLD2026-009_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-010_MIOLO.pdf -o ./temp/PNLD2026-010_MIOLO_temp.pdf -rt -m '-31'  	-d ./PNLD2026-010_MIOLO/img
+edlab-pdfconverter -i PNLD2026-010_MIOLO.pdf -o ./temp/PNLD2026-010_MIOLO_temp.pdf -p 1-2 -m '-31'   	-d ./PNLD2026-010_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-011_MIOLO.pdf -o ./temp/PNLD2026-011_MIOLO_temp.pdf -rt -m '-37'  	-d ./PNLD2026-011_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-011_MIOLO.pdf -o ./temp/PNLD2026-011_MIOLO_temp2.pdf -p 1-2 -m '-37'  -d ./PNLD2026-011_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-012_MIOLO.pdf -o ./temp/PNLD2026-012_MIOLO_temp.pdf -rt -m '-31'   	-d ./PNLD2026-012_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-012_MIOLO.pdf -o ./temp/PNLD2026-012_MIOLO_temp2.pdf -p 1-2 -m '-31'  -d ./PNLD2026-012_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-013_MIOLO.pdf -o ./temp/PNLD2026-013_MIOLO_temp.pdf -rt -m '-31'   	-d ./PNLD2026-013_MIOLO/img
+edlab-pdfconverter -i PNLD2026-013_MIOLO.pdf -o ./temp/PNLD2026-013_MIOLO_temp.pdf -p 1-2 -m '-31'   	-d ./PNLD2026-013_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-014_MIOLO.pdf -o ./temp/PNLD2026-014_MIOLO_temp.pdf -rt -m '-31'   	-d ./PNLD2026-014_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-014_MIOLO.pdf -o ./temp/PNLD2026-014_MIOLO_temp2.pdf -p 1-2 -m '-31'  -d ./PNLD2026-014_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-016_MIOLO.pdf -o ./temp/PNLD2026-016_MIOLO_temp.pdf -rt -m '-36' 	-d ./PNLD2026-016_MIOLO/img
 # edlab-pdfconverter -i PNLD2026-016_MIOLO.pdf -o ./temp/PNLD2026-016_MIOLO_temp2.pdf -p 1-2 -m '-36'  -d ./PNLD2026-016_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-022_MIOLO.pdf -o ./temp/PNLD2026-022_MIOLO_temp.pdf -rt -m '-31'  	-d ./PNLD2026-022_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-022_MIOLO.pdf -o ./temp/PNLD2026-022_MIOLO_temp2.pdf -p 1-2 -m '-31'  -d ./PNLD2026-022_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-025_MIOLO.pdf -o ./temp/PNLD2026-025_MIOLO_temp.pdf -rt -m '-31'  	-d ./PNLD2026-025_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-025_MIOLO.pdf -o ./temp/PNLD2026-025_MIOLO_temp2.pdf -p 1-2 -m '-31'  -d ./PNLD2026-025_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-026_MIOLO.pdf -o ./temp/PNLD2026-026_MIOLO_temp.pdf -rt -m '-31'  	-d ./PNLD2026-026_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-026_MIOLO.pdf -o ./temp/PNLD2026-026_MIOLO_temp2.pdf -p 1-2 -m '-31'  -d ./PNLD2026-026_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-027_MIOLO.pdf -o ./temp/PNLD2026-027_MIOLO_temp.pdf 				  	-d ./PNLD2026-027_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-028_MIOLO.pdf -o ./temp/PNLD2026-028_MIOLO_temp.pdf -rt -m '-31'   	-d ./PNLD2026-028_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-028_MIOLO.pdf -o ./temp/PNLD2026-028_MIOLO_temp2.pdf -p 1-2 -m '-31'  -d ./PNLD2026-028_MIOLO/img
+edlab-pdfconverter -i PNLD2026-030_MIOLO.pdf -o ./temp/PNLD2026-030_MIOLO_temp.pdf -rt -m '-37'   	-d ./PNLD2026-030_MIOLO/img
+edlab-pdfconverter -i PNLD2026-030_MIOLO.pdf -o ./temp/PNLD2026-030_MIOLO_temp2.pdf -p 1-2 -m '-37'  -d ./PNLD2026-030_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-031_MIOLO.pdf -o ./temp/PNLD2026-031_MIOLO_temp.pdf -rt -m '-31'   	-d ./PNLD2026-031_MIOLO/img
+edlab-pdfconverter -i PNLD2026-031_MIOLO.pdf -o ./temp/PNLD2026-031_MIOLO_temp.pdf -p 1-2 -m '-31'   	-d ./PNLD2026-031_MIOLO/img
+edlab-pdfconverter -i PNLD2026-033_MIOLO.pdf -o ./temp/PNLD2026-033_MIOLO_temp.pdf -p 1-4 -m '-31'  	-d ./PNLD2026-033_MIOLO/img
+edlab-pdfconverter -i PNLD2026-033_MIOLO.pdf -o ./temp/PNLD2026-033_MIOLO_temp.pdf -rt -p 5-37 -m '-31'  	-d ./PNLD2026-033_MIOLO/img
+edlab-pdfconverter -i PNLD2026-033_MIOLO.pdf -o ./temp/PNLD2026-033_MIOLO_temp.pdf -p 38-40 -m '-31'  	-d ./PNLD2026-033_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-034_MIOLO.pdf -o ./temp/PNLD2026-034_MIOLO_temp.pdf -rt -m '-31'   	-d ./PNLD2026-034_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-034_MIOLO.pdf -o ./temp/PNLD2026-034_MIOLO_temp2.pdf -p 1-2 -m '-31'  -d ./PNLD2026-034_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-035_MIOLO.pdf -o ./temp/PNLD2026-035_MIOLO_temp.pdf -rt -m '-37'    	-d ./PNLD2026-035_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-035_MIOLO.pdf -o ./temp/PNLD2026-035_MIOLO_temp2.pdf -p 1-2 -m '-37'  -d ./PNLD2026-035_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-036_MIOLO.pdf -o ./temp/PNLD2026-036_MIOLO_temp.pdf -rt -m '-31'  	-d ./PNLD2026-036_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-036_MIOLO.pdf -o ./temp/PNLD2026-036_MIOLO_temp2.pdf -p 1-2 -m '-31'  -d ./PNLD2026-036_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-037_MIOLO.pdf -o ./temp/PNLD2026-037_MIOLO_temp.pdf -rt -m '-37'  	-d ./PNLD2026-037_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-037_MIOLO.pdf -o ./temp/PNLD2026-037_MIOLO_temp2.pdf -p 1-2 -m '-37'  -d ./PNLD2026-037_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-038_MIOLO.pdf -o ./temp/PNLD2026-038_MIOLO_temp.pdf -rt -m '-31'  	-d ./PNLD2026-038_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-038_MIOLO.pdf -o ./temp/PNLD2026-038_MIOLO_temp2.pdf -p 1-2 -m '-31'  -d ./PNLD2026-038_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-039_MIOLO.pdf -o ./temp/PNLD2026-039_MIOLO_temp.pdf -rt -m '-37'  	-d ./PNLD2026-039_MIOLO/img
+edlab-pdfconverter -i PNLD2026-039_MIOLO.pdf -o ./temp/PNLD2026-039_MIOLO_temp2.pdf -p 1-3 -m '-37'  -d ./PNLD2026-039_MIOLO/img
+edlab-pdfconverter -i PNLD2026-042_MIOLO.pdf -o ./temp/PNLD2026-042_MIOLO_temp.pdf -rt -m '-37'  	-d ./PNLD2026-042_MIOLO/img
+edlab-pdfconverter -i PNLD2026-042_MIOLO.pdf -o ./temp/PNLD2026-042_MIOLO_temp.pdf -p 1-2 -m '-37'  	-d ./PNLD2026-042_MIOLO/img
+edlab-pdfconverter -i PNLD2026-044_MIOLO.pdf -o ./temp/PNLD2026-044_MIOLO_temp.pdf -rt -m '-37'  	-d ./PNLD2026-044_MIOLO/img
+edlab-pdfconverter -i PNLD2026-044_MIOLO.pdf -o ./temp/PNLD2026-044_MIOLO_temp.pdf -p 1-2 -m '-37'  	-d ./PNLD2026-044_MIOLO/img
+edlab-pdfconverter -i PNLD2026-045_MIOLO.pdf -o ./temp/PNLD2026-045_MIOLO_temp.pdf -rt -m '-31'  	-d ./PNLD2026-045_MIOLO/img
+edlab-pdfconverter -i PNLD2026-045_MIOLO.pdf -o ./temp/PNLD2026-045_MIOLO_temp.pdf -p 1-2 -m '-31'  	-d ./PNLD2026-045_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-046_MIOLO.pdf -o ./temp/PNLD2026-046_MIOLO_temp.pdf -rt -m '-37'  	-d ./PNLD2026-046_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-046_MIOLO.pdf -o ./temp/PNLD2026-046_MIOLO_temp2.pdf -p 1-2 -m '-37'  -d ./PNLD2026-046_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-047_MIOLO.pdf -o ./temp/PNLD2026-047_MIOLO_temp.pdf -rt -m '-37' 	-d ./PNLD2026-047_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-047_MIOLO.pdf -o ./temp/PNLD2026-047_MIOLO_temp.pdf -rt -m '-37'   	-d ./PNLD2026-047_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-047_MIOLO.pdf -o ./temp/PNLD2026-047_MIOLO_temp2.pdf -p 1-2 -m '-37'  -d ./PNLD2026-047_MIOLO/img# 
 # edlab-pdfconverter -i PNLD2026-047_MIOLO.pdf -o ./temp/PNLD2026-047_MIOLO_temp2.pdf -p 1-4 -m '-37'  -d ./PNLD2026-047_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-048_MIOLO.pdf -o ./temp/PNLD2026-048_MIOLO_temp.pdf -rt  			-d ./PNLD2026-048_MIOLO/img
 # edlab-pdfconverter -i PNLD2026-048_MIOLO.pdf -o ./temp/PNLD2026-048_MIOLO_temp2.pdf -p 1-2  		 -d ./PNLD2026-048_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-049_MIOLO.pdf -o ./temp/PNLD2026-049_MIOLO_temp.pdf -rt 				-d ./PNLD2026-049_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-049_MIOLO.pdf -o ./temp/PNLD2026-049_MIOLO_temp2.pdf -p 1-3 			 -d ./PNLD2026-049_MIOLO/img
+# edlab-pdfconverter -i PNLD2026-050_MIOLO.pdf -o ./temp/PNLD2026-050_MIOLO_temp.pdf -rt  			-d ./PNLD2026-050_MIOLO/img
 # edlab-pdfconverter -i PNLD2026-050_MIOLO.pdf -o ./temp/PNLD2026-050_MIOLO_temp2.pdf -p 1-2  		 -d ./PNLD2026-050_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-002_MIOLO.pdf -o ./temp/PNLD2026-002_MIOLO_temp2.pdf -p 1-2  		 -d ./PNLD2026-002_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-003_MIOLO.pdf -o ./temp/PNLD2026-003_MIOLO_temp2.pdf -p 1-2 -m '-40' -d ./PNLD2026-003_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-004_MIOLO.pdf -o ./temp/PNLD2026-004_MIOLO_temp2.pdf -p 1-2   		  -d ./PNLD2026-004_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-009_MIOLO.pdf -o ./temp/PNLD2026-009_MIOLO_temp2.pdf -p 1-2 -m '-37' -d ./PNLD2026-009_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-011_MIOLO.pdf -o ./temp/PNLD2026-011_MIOLO_temp2.pdf -p 1-2 -m '-37' -d ./PNLD2026-011_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-012_MIOLO.pdf -o ./temp/PNLD2026-012_MIOLO_temp2.pdf -p 1-2 -m '-31' -d ./PNLD2026-012_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-014_MIOLO.pdf -o ./temp/PNLD2026-014_MIOLO_temp2.pdf -p 1-2 -m '-31' -d ./PNLD2026-014_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-022_MIOLO.pdf -o ./temp/PNLD2026-022_MIOLO_temp2.pdf -p 1-2 -m '-31' -d ./PNLD2026-022_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-025_MIOLO.pdf -o ./temp/PNLD2026-025_MIOLO_temp2.pdf -p 1-2 -m '-31' -d ./PNLD2026-025_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-026_MIOLO.pdf -o ./temp/PNLD2026-026_MIOLO_temp2.pdf -p 1-2 -m '-31' -d ./PNLD2026-026_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-028_MIOLO.pdf -o ./temp/PNLD2026-028_MIOLO_temp2.pdf -p 1-2 -m '-31' -d ./PNLD2026-028_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-030_MIOLO.pdf -o ./temp/PNLD2026-030_MIOLO_temp2.pdf -p 1-2 -m '-31' -d ./PNLD2026-030_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-034_MIOLO.pdf -o ./temp/PNLD2026-034_MIOLO_temp2.pdf -p 1-2 -m '-31' -d ./PNLD2026-034_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-035_MIOLO.pdf -o ./temp/PNLD2026-035_MIOLO_temp2.pdf -p 1-2 -m '-37' -d ./PNLD2026-035_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-036_MIOLO.pdf -o ./temp/PNLD2026-036_MIOLO_temp2.pdf -p 1-2 -m '-31' -d ./PNLD2026-036_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-037_MIOLO.pdf -o ./temp/PNLD2026-037_MIOLO_temp2.pdf -p 1-2 -m '-37' -d ./PNLD2026-037_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-038_MIOLO.pdf -o ./temp/PNLD2026-038_MIOLO_temp2.pdf -p 1-2 -m '-31' -d ./PNLD2026-038_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-039_MIOLO.pdf -o ./temp/PNLD2026-039_MIOLO_temp2.pdf -p 1-2 -m '-37' -d ./PNLD2026-039_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-046_MIOLO.pdf -o ./temp/PNLD2026-046_MIOLO_temp2.pdf -p 1-2 -m '-37' -d ./PNLD2026-046_MIOLO/img
-# edlab-pdfconverter -i PNLD2026-047_MIOLO.pdf -o ./temp/PNLD2026-047_MIOLO_temp2.pdf -p 1-2 -m '-37' -d ./PNLD2026-047_MIOLO/img# 
 
-# # intervenção manual necessária: PNLD2026-037
-# # sem intervenção: PNLD2026-006# 
-
-# # Mudanças no script 
-# # 1. verificar se arquivo existe
-# # 2. Salvar com um nome default caso não esteja definido -o
-# # 3. "p 1-end" e "-p 1,3,4"
-# # 4. Corrigir mensagens de erro e README.md 
